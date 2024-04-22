2fe2c76de9ace6f4e7a47e963ffff375308d929f
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2024-04-22 10:45:07 -0300

    exemplos de tipos não numéricos bool e char

--- Fundamentos/CSFundamentos1/TiposDeDados1/obj/Debug/net6.0/TiposDeDados1.AssemblyInfo.cs
+++ Fundamentos/CSFundamentos1/TiposDeDados1/obj/Debug/net6.0/TiposDeDados1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiposDeDados1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ee6dd646147804b83e58f870a03c79f74036f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ee2e86f4cffd43ee8a83e5fca8a0e6d92778a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiposDeDados1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiposDeDados1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
