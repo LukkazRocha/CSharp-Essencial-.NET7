650c07a3226c8493053a7c0aa5a5c5d135dd9c87
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2024-04-27 15:57:35 -0300

    tipo dateTime

---
+++ Fundamentos/CSFundamentos1/NullableTypes1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("## Nullable Types ##\n");
+
+int valor = 100;
+Console.WriteLine(valor);
+
+Console.ReadKey();
--- Fundamentos/CSFundamentos1/TiposDeDados2/obj/Debug/net6.0/TiposDeDados2.AssemblyInfo.cs
+++ Fundamentos/CSFundamentos1/TiposDeDados2/obj/Debug/net6.0/TiposDeDados2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiposDeDados2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe2c76de9ace6f4e7a47e963ffff375308d929f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2376738aa3efd4e757db19987426ba6a92926970")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiposDeDados2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiposDeDados2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
