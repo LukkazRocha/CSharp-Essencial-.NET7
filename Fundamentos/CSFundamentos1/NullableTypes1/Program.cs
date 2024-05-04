d34bf5477cfa9e95cfa79722053a71128e2c9227
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2024-05-04 17:12:29 -0300

    estudos sobre nullable

--- Fundamentos/CSFundamentos1/NullableTypes1/Program.cs
+++ Fundamentos/CSFundamentos1/NullableTypes1/Program.cs
@@ -1,6 +1,38 @@
 ﻿Console.WriteLine("## Nullable Types ##\n");
 
-int valor = 100;
-Console.WriteLine(valor);
+//int? i = null;
+//double? d = null;
+//bool? b = null;
+
+//Console.WriteLine(i);
+//Console.WriteLine(d);
+//Console.WriteLine(b);
+
+//////////////////////////////////////////////////
+
+//int? a = null;
+
+//int b = a ?? 0;
+
+//////////////////////////////////////////////////
+
+//int? x = 4;
+//int? y = 3;
+//int? z = x * 3;
+
+//Console.WriteLine(z);
+
+//////////////////////////////////////////////////
+
+int? b = null;
+
+if (b.HasValue)
+{
+    Console.WriteLine($"b = {b.Value}");
+}
+else
+{
+    Console.WriteLine("b não possui um valor (null)");
+}
 
 Console.ReadKey();
--- Fundamentos/CSFundamentos1/NullableTypes1/obj/Debug/net6.0/NullableTypes1.AssemblyInfo.cs
+++ Fundamentos/CSFundamentos1/NullableTypes1/obj/Debug/net6.0/NullableTypes1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NullableTypes1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2376738aa3efd4e757db19987426ba6a92926970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+650c07a3226c8493053a7c0aa5a5c5d135dd9c87")]
 [assembly: System.Reflection.AssemblyProductAttribute("NullableTypes1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NullableTypes1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
