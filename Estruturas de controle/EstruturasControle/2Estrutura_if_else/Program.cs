a3d9ba78ae7c0621f49b3dfbebafabf281ad93b5
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2024-06-15 16:31:16 -0300

    inicia modulo sobre estruturas de controles

---
+++ Estruturas de controle/EstruturasControle/2Estrutura_if_else/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("## Instrução if-else ##\n");
+
+Console.WriteLine("Informe o valor de x\t");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o valor de y\t");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("x é maior que y");
+}
+else
+{
+    if (x < y)
+    {
+        Console.WriteLine("x é menor que y");
+    }
+    else
+    {
+        Console.WriteLine("x é igual a y");
+    }
+}
+
+Console.WriteLine("Fim do processamento...");
+Console.ReadKey();
