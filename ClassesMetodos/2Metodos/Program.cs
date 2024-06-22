e6d4467cf36237f302875b1755e7287c887b174d
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2024-06-22 16:54:40 -0300

    iniciando módulo "Classes e Métodos"

---
+++ ClassesMetodos/2Metodos/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## Métodos ##");
+
+MinhaClasse minhaClasse = new MinhaClasse();
+
+minhaClasse.Saudacao();
+//minhaClasse.ExibirDataAtual();
+
+Console.ReadKey();
+
+class MinhaClasse
+{
+    public void Saudacao()
+    {
+        Console.WriteLine("Bem-Vindo");
+        ExibirDataAtual();
+    }
+
+    public void ExibirDataAtual() => Console.WriteLine(DateTime.Now.ToShortDateString());
+}
---
+++ ClassesMetodos/3MetodosParametros/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("## Métodos ##\n");
+
+MinhaClasse minhaClasse = new();
+var nomeCliente = "Maria";
+var dataAtual = DateTime.Now.ToShortDateString();
+
+//minhaClasse.Saudacao("Maria", DateTime.Now.ToShortDateString());
+
+minhaClasse.Saudacao(nomeCliente, dataAtual);
+
+Console.ReadKey();
+
+public class MinhaClasse
+{
+    public void Saudacao(string nome, string data)
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine(data);
+    }
+}
