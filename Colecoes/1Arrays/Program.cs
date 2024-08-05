841785752df5c5a11b7d27854a3b3044d4cfec73
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2024-08-05 13:47:39 -0300

    inicia estudos relacionados à arrays e coleções

---
+++ Colecoes/1Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("### Arrays ###\n");
+
+// Declaração do array
+int[] numeros;
+
+// Aloquei memória
+numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//string[] nomes = new string[5] { "Ana", "Maria", "Marta", "Paulo", "Carlos" };
+
+//string[] nomes1 = new string[] { "Ana", "Maria", "Marta", "Paulo", "Carlos" };
+
+string[] nomes = { "Ana", "Maria", "Marta", "Paulo", "Carlos" };
+
+Console.WriteLine(nomes[0]);
+Console.WriteLine(nomes[1]);
+Console.WriteLine(nomes[2]);
+Console.WriteLine(nomes[3]);
+Console.WriteLine(nomes[4]);
+
+// declarando um array
+int[] numeros1;
+
+// alocando memória
+numeros1 = new int[3];
+
+numeros1[0] = 1;
+numeros1[1] = 2;
+numeros1[2] = 3;
+
+Console.ReadKey();
