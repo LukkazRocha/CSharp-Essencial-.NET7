301a3d7d3dfd58e76f65779cd82410e83451f95d
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2025-01-25 15:33:39 -0300

    fix

---
+++ DelegatesLambdasEventosELINQ/01Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using _01Delegates;
+
+var resultado = Calculadora.Somar(10, 40);
+Console.WriteLine(resultado);
+
+DelegateCalculadora calc = Calculadora.Somar;
+var resultado1 = calc.Invoke(20, 30);
+Console.WriteLine($"Soma = {resultado1}");
+
+calc = Calculadora.Dividir;
+var resultado2 = calc.Invoke(20, 30);
+Console.WriteLine($"Dividir = {resultado2}");
+
+calc = Calculadora.Subtrair;
+var resultado3 = calc.Invoke(20, 30);
+Console.WriteLine($"Subtrair = {resultado3}");
+
+calc = Calculadora.Multiplicar;
+var resultado4 = calc.Invoke(20, 30);
+Console.WriteLine($"Multiplicar = {resultado4}");
+
+Console.ReadKey();
+
+public delegate float DelegateCalculadora(float x, float y);
---
+++ TratamentoDeErros/01TryCatchFinally/Program.cs
@@ -0,0 +1,30 @@
+﻿
+try
+{
+    Console.WriteLine("\nInforme o dividendo");
+    int dividendo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("\nInforme o divisor");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+
+    int resultado = dividendo / divisor;
+    Console.WriteLine($"\n{dividendo} / {divisor} = {resultado}\n");
+
+    Console.ReadKey();
+}
+catch (FormatException)
+{
+    Console.WriteLine("\nInforme um valor inteiro");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("\nInforme um valor inteiro entre 1 e 9999999");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("\nNão existe divisão por zero, tente novamente");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
