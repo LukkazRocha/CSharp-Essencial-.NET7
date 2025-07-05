09bfc553c1ce484ff8818e917f71258e679b8ed0
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2025-07-05 15:14:52 -0300

    multicast delegate

--- DelegatesLambdasEventosELINQ/01Delegates/obj/Debug/net7.0/01Delegates.AssemblyInfo.cs
+++ DelegatesLambdasEventosELINQ/01Delegates/obj/Debug/net7.0/01Delegates.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01Delegates")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d9de02f2f092a8247c8268aefab9350bf117c0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b979b4767da182df07b1a8e7668469409df2b01")]
 [assembly: System.Reflection.AssemblyProductAttribute("01Delegates")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01Delegates")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ DelegatesLambdasEventosELINQ/02DelegateMulticast/Program.cs
@@ -0,0 +1,24 @@
+﻿
+MeuDelegate delegateMulticast = new MeuDelegate(Metodo1);
+delegateMulticast += Metodo2;
+
+delegateMulticast("Olá, multicast delegate!");
+
+Console.ReadKey();
+
+static void Metodo1(string mensagem)
+{
+    Console.WriteLine("Método 1: " + mensagem);
+}
+
+static void Metodo2(string mensagem)
+{
+    Console.WriteLine("Método 2: " + mensagem);
+}
+
+static void Metodo2(string mensagem)
+{
+    Console.WriteLine("Método 2: " + mensagem);
+}
+
+public delegate void MeuDelegate(string mensagem);
